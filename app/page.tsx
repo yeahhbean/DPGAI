39dd2f543603107355c5a2f4ef02edfcef09e72b
gdkds12 <gdkds12@gmail.com>
2024-08-30 11:41:20 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
+
+
+
+            
             By{" "}
             <Image
               src="/vercel.svg"
